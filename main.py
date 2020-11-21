5a9bc601f5123623d341dc2681c86afe93e9200e
Kelly Foulk <klfoulk16@gmail.com>
2020-11-21 16:05:57 -0500

    finished backtrack, choose_unassigned_var, and revise_domains

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from sudoku import *
 class PuzzleManager():
     def __init__(self, sudoku):
         self.sudoku = sudoku
-        self.domains = {(variable.h, variable.w): variable.domain for variable in self.sudoku.variables}
         self.original_assignment = {variable: variable.domain[0] for variable in self.sudoku.variables if len(variable.domain) == 1}
 
     def solve(self):
@@ -20,6 +19,40 @@ class PuzzleManager():
                 if self.original_assignment[var] in var2.domain:
                     var2.domain.remove(self.original_assignment[var])
 
+    def backtrack(self, assignment=None):
+        """Takes an assignment as an arguement. To start the assignment will be the origin mapping of the pre-set variables (specify this). Later it will be the assignment that the algorithm determines. If the assignment contains every single variable in the problem, then it returns the assignment. Otherwise...
+
+        Choose a **random unassigned variable, then it'll iteratively check every value in the domain of the variable to see if the value is arc consistent with the current assignment. If it is, then {variable= value} is added to the assignment dictionary. Then call backtrack on that assignment. If the result was not failure, then return the result assignment. Otherwise, remove {variable=value} from the assignment and try the next value in the variable's domain.
+
+        If we reach the end of the values in the variable's domain return failure. Thus we will back up a step in the backtracking heirarchy because some earlier assignment is causing issues.
+        """
+
+        if not assignment:
+            assignment = self.original_assignment
+
+        var = self.choose_unassigned_variable(assignment)
+        for value in var.domain:
+            if self.check_arc_consistency(assignment, var):
+                assignment[var] = value
+                if backtrack(assignment):
+                    return assignment
+                else:
+                    del assignment[var]
+        return None
+
+    def choose_unassigned_variable(self, assignment):
+        """For now this will just return a random variable. Eventually we will use a heuristic to return the hopefully best variable to check given the current assignment."""
+
+        for var in self.sudoku.variables:
+            if var not in assignment.keys():
+                return var
+
+    def check_arc_consistency(self, assignment, var):
+        """For any given assignment check to see if all the values are arc-consistent with each other.
+
+        This is necessary. If we called arc3 instead of this, we would be removing numbers from each variable's domain with each backtrack call, which would cause a problem if we had to "un"backtrack. The numbers wouldn't be readded and the domain would be inaccurate."""
+        # see if all blocks, rows and columns do not have duplicates
+
     def depricated_revise_domains(self, x, y):
         """DEPRECATED Revise X's domain to enforce arc consistency between X and Y. For each value in X's domain, check to see if there is a variable in Y's domain that satisfies constraint (X,Y). If not remove variable. If no variables were removed from X's domain return False, else return True.
 
@@ -32,7 +65,7 @@ class PuzzleManager():
 
         raise NotImplementedError
 
-    def arc3(self):
+    def depricated_arc3(self):
         """DEPRECATED Maintain a queue of all arcs in puzzle (or arcs that need checking). As long as the queue is not empty, remove (X,Y) from the queue and run Revise().
 
         If revise is true, check to see if the domain of X is 0. If so return false because it's not possible to solve the problem. Otherwise, add every arc including X (aside from the one we just checked) to the queue.
@@ -57,28 +90,6 @@ class PuzzleManager():
                 #queue.add()
         raise NotImplementedError
         return true
-
-
-    def backtrack(self, assignment=None):
-        """Takes an assignment as an arguement. To start the assignment will be the origin mapping of the pre-set variables (specify this). Later it will be the assignment that the algorithm determines. If the assignment contains every single variable in the problem, then it returns the assignment. Otherwise...
-
-        Choose a **random unassigned variable, then it'll iteratively check every value in the domain of the variable to see if the value is arc consistent with the current assignment. If it is, then {variable= value} is added to the assignment dictionary. Then call backtrack on that assignment. If the result was not failure, then return the result assignment. Otherwise, remove {variable=value} from the assignment and try the next value in the variable's domain.
-
-        If we reach the end of the values in the variable's domain return failure. Thus we will back up a step in the backtracking heirarchy because some earlier assignment is causing issues.
-        """
-        raise NotImplementedError
-
-    def choose_unassigned_variable(self, assignment):
-        """For now this will just return a random variable. Eventually we will use a heuristic to return the hopefully best variable to check given the current assignment."""
-        raise NotImplementedError
-
-    def check_arc_consistency(self, assignment):
-        """For any given assignment check to see if all the values are arc-consistent with each other.
-
-        I think this is unneeded and actually what we want to use is the arc3 function. Unless I want to use this as a final check for debugging purposes?
-
-        This is necessary. If we called arc3 instead of this, we would be removing numbers from each variable's domain with each backtrack call, which would cause a problem if we had to "un"backtrack. The numbers wouldn't be readded and the domain would be inaccurate."""
-        raise NotImplementedError
 
 def main():
     # check usage
